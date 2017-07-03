6d92afc4e935d830602348de38284a2508d2d2de
ggcchhrr <ggcchhrr@gmail.com>
2017-07-03 14:30:13 -0400

    rename files

--- ch04-functional-program/fun_partial.py
+++ ch04-functional-program/fun_partial.py
@@ -9,3 +9,11 @@ import functools
 int2_partial = functools.partial(int, base=2)
 x2 = int2_partial('1000')
 print('x2 =', x2)
+
+kw1 = {'base':2}
+x3 = int('10100', **kw1)
+print('x3 =', x3)
+
+f4_max2 = functools.partial(max,10)
+x4 = f4_max2(5,6,7)
+print("x4 =", x4)
---
+++ ch05-model/use_model.py
@@ -0,0 +1,20 @@
+# use model
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'A test module'
+
+__author__ = 'Charles Guo'
+
+import sys
+def test():
+	args = sys.argv
+	if len(args) == 1:
+		print("Hello World!")
+	elif len(args) == 2:
+		print("Hello %s" %(args[1]))
+	else:
+		print("Too many args!")
+
+if __name__ == "__main__":
+	test()
